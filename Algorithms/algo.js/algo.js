62578f2baba4edd4423840a9e35d5f9e96f56498
malek774 <malekd800@gmail.com>
2023-06-23 20:25:43 +0100

    hello

---
+++ Algorithms/algo.js/algo.js
@@ -0,0 +1,17 @@
+var arr = ["a", "b", "c", "d", "e"];
+
+function reverse (array)
+// Code goes here 
+var newArray =[]
+for(var i= array length -1;i>1;i-- )
+newArray push (array)[i] 
+}
+return newArray
+}
+console.log(reverse2(arr))
+
+
+// 
+
+console.log ("REVERSE TWO ",reverse2)
+
